5630a351fa25fa5ddc0826c5ae151554f0ff40ff
marshall405 <marshall.slemp@gmail.com>
2020-10-23 19:01:38 -0500

    wraps js in iife and changes input to stick

--- main.js
+++ main.js
@@ -1,62 +1,65 @@
-let dataSet = [423, 1234, 1535, 1463]
-const code = document.getElementById('code')
+(function () {
+    let dataSet = [423, 1234, 1535, 1463]
+    const code = document.getElementById('code')
 
-document.getElementById('data').addEventListener('keydown', (e) => {
-    if (e.keyCode === 13) {
-        // Enter
-        addData()
-    }
-})
-document.getElementById('addData').addEventListener('click', () => {
-    addData()
-})
-document.getElementById('clear').addEventListener('click', () => {
-    dataSet = []
-    renderData()
-})
-
-function addData() {
-    const data = document.getElementById('data')
-    if (Number(data.value)) {
-        dataSet.push(Number(data.value))
-        renderData()
-    } else {
-        alert('Data must be a number!')
-    }
-    data.focus()
-    data.value = ""
-}
-const div = d3.select("#chart")
-
-let width = parseInt(window.getComputedStyle(document.getElementById('chart')).width)
-window.addEventListener('resize', () => {
-    width = parseInt(window.getComputedStyle(document.getElementById('chart')).width)
-    renderData()
-})
-
-function renderData() {
-    const x = d3.scaleLinear()
-        .domain([0, d3.max(dataSet)])
-        .range([1, width - 40])
-    dataSet.sort((a, b) => a - b)
-    div.selectAll('div')
-        .data(dataSet)
-        .join('div')
-        .style('width', d => `${x(d)}px`)
-        .text(d => d);
-    updateCode()
-}
-
-renderData()
-
-
-function updateCode() {
-
-    let innerText = '<div class="chart-container">'
-    document.querySelectorAll('#chart div').forEach(div => {
-        innerText += `<div style="width: ${div.style.width}">${div.innerText}</div>`
+    document.getElementById('data').addEventListener('keydown', (e) => {
+        if (e.keyCode === 13) {
+            // Enter
+            addData()
+        }
     })
-    innerText += '</div>'
-    code.innerText = innerText
+    document.getElementById('addData').addEventListener('click', () => {
+        addData()
+    })
+    document.getElementById('clear').addEventListener('click', () => {
+        dataSet = []
+        renderData()
+    })
 
-}
+    function addData() {
+        const data = document.getElementById('data')
+        if (Number(data.value)) {
+            dataSet.push(Number(data.value))
+            renderData()
+        } else {
+            alert('Data must be a number!')
+        }
+        data.focus()
+        data.value = ""
+    }
+    const div = d3.select("#chart")
+
+    let width = parseInt(window.getComputedStyle(document.getElementById('chart')).width)
+    window.addEventListener('resize', () => {
+        width = parseInt(window.getComputedStyle(document.getElementById('chart')).width)
+        renderData()
+    })
+
+    function renderData() {
+        const x = d3.scaleLinear()
+            .domain([0, d3.max(dataSet)])
+            .range([1, width - 40])
+        dataSet.sort((a, b) => a - b)
+        div.selectAll('div')
+            .data(dataSet)
+            .join('div')
+            .style('width', d => `${x(d)}px`)
+            .text(d => d);
+        updateCode()
+    }
+
+    renderData()
+
+
+    function updateCode() {
+
+        let innerText = '<div class="chart-container">'
+        document.querySelectorAll('#chart div').forEach(div => {
+            innerText += `<div style="width: ${div.style.width}">${div.innerText}</div>`
+        })
+        innerText += '</div>'
+        code.innerText = innerText
+
+    }
+
+})()
